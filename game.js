0929631d6639bc4cedfcdf2c2f48929ec763237a
tmedryk <tomekmedryk@gmail.com>
2021-01-20 21:36:00 +0100

    bombs!!

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-import { drawFood, drawSnake } from './view.js'
+import { drawFood, drawSnake, drawBomb } from './view.js'
 import { moveSnake } from './control.js'
 
 let speed = 100
@@ -7,13 +7,14 @@ let gameBox = document.getElementById('box')
 let scorepoints = document.getElementById('points')
 let snakeLength = 3
 let foodSpot = { x: 0, y: 0 }
+let bombSpot = { x: 1, y: 1 }
 let foodcounter = 0
 initGame()
 
 
 function initGame() {
   let snakeHead = snakeBody[snakeBody.length - 1];
-  if (snakeDeath(snakeHead, snakeBody)) {
+  if (snakeDeath(snakeHead, snakeBody, bombSpot)) {
     alert('Game over' +" Your score is: "+ foodcounter +" points")
   }
   else {
@@ -22,6 +23,7 @@ function initGame() {
     scorepoints.innerHTML = points();
     drawSnake(snakeBody, gameBox)
     foodSpot = drawFood(snakeBody, gameBox, foodSpot)
+    bombSpot = drawBomb(snakeBody, gameBox, foodSpot, bombSpot)
     eatFood(snakeHead)
     setTimeout(initGame, speed)
   }
@@ -37,7 +39,7 @@ function eatFood(snakeHead) {
   if (snakeHead.x == foodSpot.x && snakeHead.y == foodSpot.y) {
     snakeLength += 1;
     foodSpot = { x: 0, y: 0 };
-    speed = speed - speed / 4;
+    speed = speed - speed / 10;
     foodcounter++
     // console.log("points", foodcounter)
     return foodcounter
@@ -45,7 +47,7 @@ function eatFood(snakeHead) {
 
 }
 
-function snakeDeath(snakeHead, snakeBody) {
+function snakeDeath(snakeHead, snakeBody, bombSpot) {
   if (snakeBody.length > 4) {
     let headlessSnake = snakeBody.slice(0, snakeBody.length - 1);
     for (let part of headlessSnake) {
@@ -56,6 +58,11 @@ function snakeDeath(snakeHead, snakeBody) {
   }
   if (snakeHead.x < 1 || snakeHead.x > 25 || snakeHead.y < 1 || snakeHead.y > 25) {
     return true
+  }
+
+  if (snakeHead.x == bombSpot.x && snakeHead.y == bombSpot.y) {
+    return true
+
   }
 
   return false
